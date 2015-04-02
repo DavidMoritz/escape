11782b840093eb0129c5c70deb42d0ed11135c47
Moritz, David <Moritz.David@principal.com>
2015-04-02 17:08:57 -0500

    add timer functionality

--- src/trapped.js
+++ src/trapped.js
@@ -2,26 +2,36 @@ trappedApp.controller('TrappedCtrl', [
 	'$scope',
 	'$interval',
 	'$timeout',
-	'$http',
-	function TrappedCtrl($s, $interval, $timeout, $http) {
+	function TrappedCtrl($s, $interval, $timeout) {
 		'use strict';
 
-		function Hello($s, $http) {
-			$http.get('http://rest-service.guides.spring.io/greeting')
-				.success(function getData(data) {
-					$s.greeting = data;
-				});
-		}
+		var timer = 60 * 60; // one hour in seconds
 
 		//	initialize scoped variables
 		_.assign($s, {
 			clues: 0,
-			time: 60 * 60 * 1000,
-			gameStarted: false
+			gameStarted: false,
+			displayTime: '60:00'
 		});
 
-		$s.startGame = function startGame() {
-			this.Hello(this, $http);
+		function subtractSecond($s) {
+			var minutes = Math.floor((--timer % 3600) / 60),
+				seconds = timer - (minutes * 60);
+
+			seconds = seconds < 10 ? ('0' + seconds) : seconds;
+			minutes = minutes < 10 ? ('0' + minutes) : minutes;
+
+			$s.displayTime = minutes + ':' + seconds;
 		}
+
+		$interval(function everySecond() {
+			if ($s.gameStarted) {
+				subtractSecond($s);
+			}
+		}, 1000, $s.timer);
+
+		$s.startGame = function startGame() {
+			$s.gameStarted = true;
+		};
 	}
 ]);
