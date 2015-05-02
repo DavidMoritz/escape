6f3dcb53ac1eae5add94bafc5ed1c911674a6af9
Jeremy Moritz <jeremy@jeremymoritz.com>
2015-05-02 16:29:04 -0500

    first question refactored

--- src/js/session.js
+++ src/js/session.js
@@ -37,13 +37,19 @@ escapeApp.controller('SessionCtrl', [
 			teamId: null,
 			curMsg: '',
 			timeRemaining: 0,
-			q25: {
-				id: 25,
-				guess: {
-					property: '',
-					money: ''
-				},
-				answers: ['Illinois10']
+			q: {
+				monopoly: {
+					id: 25,
+					guess: '',
+					splitGuess: {
+						property: '',
+						money: ''
+					},
+					answers: ['Illinois10'],
+					solved: false,
+					attempts: [],
+					nextClue: 'Cerulean (light blue) lock: 12345'
+				}
 			}
 		});
 
@@ -65,31 +71,33 @@ escapeApp.controller('SessionCtrl', [
 				return ans.toLowerCase();
 			});
 
+			q.attempts.push({
+				guess: q.guess,
+				time: moment().format(timeFormat)
+			});
+
 			if (_.contains(lowerCaseAnswers, q.guess.toLowerCase())) {
-				alert('correct!');
+				q.solved = true;
 			} else {
-				alert('nope!  the correct answer is ' + q.answers[0]);
+				alert('Incorrect answer.  Try again!');
 			}
 		};
 
 		//	some guesses need to be massaged before submitting them
 		$s.submitGuessSpecial = function submitGuessSpecial(qNum) {
 			if (qNum.id === 25) {
-				var property = _.contains(qNum.guess.property, 'Illinois') ? 'Illinois' : qNum.guess.property;
-				var money = qNum.guess.money.replace(/\$/g, '').replace('.00', '');
-				var reformattedQuestion = {
-					guess: property + money,
-					answers: qNum.answers
-				};
+				var property = _.contains(qNum.splitGuess.property, 'Illinois') ? 'Illinois' : qNum.splitGuess.property;
+				var money = qNum.splitGuess.money.replace(/\$/g, '').replace('.00', '');
 
-				$s.submitGuess(reformattedQuestion);
+				qNum.guess = property + money;
+				$s.submitGuess(qNum);
 			} else {
 				console.log('error');
 			}
 		};
 
-		$s.getPlaceholder = function getPlaceholder(question) {
-			return question.placeholder || 'Answer here';
+		$s.revealNextClue = function revealNextClue(q) {
+			alert(q.nextClue);
 		};
 
 		$s.allTeams = EF.getFBArray('teams');
