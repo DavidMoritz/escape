d964cc0a9868f58676e2a6a2a2fa451b947a81dd
Moritz, David <Moritz.David@principal.com>
2015-05-22 17:48:16 -0500

    make changes to landing page and add medal logic

--- src/js/public.js
+++ src/js/public.js
@@ -2,8 +2,52 @@ escapeApp.controller('EscapeCtrl', [
 	'$scope',
 	'$timeout',
 	'$interval',
-	function EscapeCtrl($s, $timeout, $interval) {
+	'EscapeFactory',
+	function EscapeCtrl($s, $timeout, $interval, EF) {
 		'use strict';
+
+		function init() {
+			//	initKeypad
+			$('.numericKeypad').keypad({
+				separator: '|',
+				layout: [
+					'1|2|3',
+					'4|5|6',
+					'7|8|9',
+					$.keypad.CLEAR + '|0|<i class="fa fa-level-down fa-rotate-90"></i>'
+				],
+				showAnim: '',
+				clearText: 'X',
+				keypadClass: 'digitalKeypad',
+				onKeypress: function(key, value, inst) {
+					var display = value;
+					// play *beep
+					for(var i = value.length; i < 5; i++) {
+						display += '-';
+					}
+					$s.guess = value;
+					$('.numberEntry').text(display);
+
+					if (key === '') {
+						$('#keypadModal').modal('hide');
+					}
+				},
+				beforeShow: function(div, inst) {
+					$('<div>', {
+						class: 'numberEntry',
+						text: '-----'
+					}).prependTo(div);
+					div.appendTo($('#keypadModal .modal-body'));
+				}
+			});
+			$('#keypadModal').on('shown.bs.modal', function() {
+				$(this).addClass('shown');
+				$('.numericKeypad').keypad('show');
+			}).on('hidden.bs.modal', function() {
+				$('.numberEntry').text('-----');
+				$s.guess = '';
+			});
+		}
 
 		function typeOutMessage() {
 			$s.public.display = '';
@@ -42,6 +86,12 @@ escapeApp.controller('EscapeCtrl', [
 			$s.gauge = Math.random() * (0.3 + 0.2) - 0.2;
 		}, 1000);
 
+		$s.requestHint = function() {
+			if($s.activeTeam.status < $s.status.length) {
+				$s.activeTeam.status++;
+			}
+		};
+
 		$s.isSolved = function(puz) {
 			if(puz) {
 				return puz.solved;
@@ -56,7 +106,8 @@ escapeApp.controller('EscapeCtrl', [
 			},
 			activeTeam: {
 				password: 'nothing',
-				passwordRequired: true
+				passwordRequired: false,
+				status: 0
 			},
 			allPuzzles: {
 				puz1: {
@@ -66,7 +117,9 @@ escapeApp.controller('EscapeCtrl', [
 				puz2: {
 					name: 'second'
 				}
-			}
+			},
+			status: EF.statuses
 		});
+		init();
 	}
 ]);
