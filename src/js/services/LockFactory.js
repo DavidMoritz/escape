2022d48828372659f7fb82f2ca7ea679173592ec
David Moritz <davidmoritz@gmail.com>
2015-06-01 20:31:01 -0500

    styling changes

--- src/js/services/LockFactory.js
+++ src/js/services/LockFactory.js
@@ -5,85 +5,85 @@ escapeApp.factory('LockFactory', [
 		return [{
 			track: 'a',
 			lockIcon: 'university',
-			code: '32-0-20',
+			code: '32 - 0 - 20',
 			opens: 'monopoly',
 			box: 'Mad Gab'
 		}, {
 			track: 'a',
 			lockIcon: 'diamond',
-			code: '2-20-38',
+			code: '2 - 20 - 38',
 			opens: 'yahtzee',
 			box: 'Gray metal box'
 		}, {
 			track: 'a',
 			lockIcon: 'paw',
-			code: '1-9-35',
+			code: '1 - 9 - 35',
 			opens: 'scrabble',
 			box: 'Gray suitcase'
 		}, {
 			track: 'a',
 			lockIcon: 'gavel',
-			code: '14-29-11',
+			code: '14 - 29 - 11',
 			opens: 'jigsaw',
 			box: 'Brown bread box'
 		}, {
 			track: 'b',
 			lockIcon: 'anchor',
-			code: '30-5-33',
+			code: '30 - 5 - 33',
 			opens: 'clue',
 			box: 'Blue dice block'
 		}, {
 			track: 'b',
 			lockIcon: 'tachometer',
-			code: '9-21-1',
+			code: '9 - 21 - 1',
 			opens: 'guessWho',
 			box: 'Balderdash'
 		}, {
 			track: 'b',
 			lockIcon: 'tree',
-			code: '24-4-27',
+			code: '24 - 4 - 27',
 			opens: 'chess',
 			box: 'Rush Hour'
 		}, {
 			track: 'b',
 			lockIcon: 'birthday-cake',
-			code: '30-38-24',
+			code: '30 - 38 - 24',
 			opens: 'crossword',
 			box: 'Tripoley'
 		}, {
 			track: 'b',
 			lockIcon: 'globe',
-			code: '18-25-7',
+			code: '18 - 25 - 7',
 			opens: 'jigsaw',
 			box: 'Scene It?'
 		}, {
 			track: 'c',
 			lockIcon: 'umbrella',
-			code: '1-15-0',
+			code: '1 - 15 - 0',
 			opens: 'connect4',
 			box: 'Cranium'
 		}, {
 			track: 'c',
 			lockIcon: 'bomb',
-			code: '23-7-26',
+			code: '23 - 7 - 26',
 			opens: 'fiveRoutes',
 			box: 'Life'
 		}, {
 			track: 'c',
 			lockIcon: 'motorcycle',
-			code: '16-21-39',
+			code: '16 - 21 - 39',
 			opens: 'wordFind',
 			box: 'Blurt'
 		}, {
 			track: 'c',
 			lockIcon: 'rocket',
-			code: '36-11-37',
+			code: '36 - 11 - 37',
 			opens: 'taboo',
 			box: 'Pictionary'
 		}, {
 			track: 'c',
 			lockIcon: 'ticket',
-			code: '11-21-37',
+			code: '11 - 21 - 37',
 			opens: 'jigsaw',
 			box: 'Trivial Pursuit'
 		}];
@@ -93,6 +93,6 @@ escapeApp.factory('LockFactory', [
 //	FYI
 // scissors (code arrived from chutes and ladders)
 // NOTE: the clue for chutes and ladders in in the umbrella locked Cranium
-// code: '28-36-16',
+// code: '28 - 36 - 16',
 // box: mancala
 // opens: on the way to connect4
