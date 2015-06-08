0c2977ae280fd30a6a0d6fc4757c06c22829ff20
DavidMoritz <davidmoritz@gmail.com>
2015-06-07 20:16:34 -0500

    redid lock factory

--- src/js/services/LockFactory.js
+++ src/js/services/LockFactory.js
@@ -6,32 +6,32 @@ escapeApp.factory('LockFactory', [
 			track: 'a',
 			lockIcon: 'university',
 			code: '32 - 0 - 20',
-			opens: 'monopoly',
+			opens: 'monopoly', // by way of trashcan
 			box: 'Mad Gab'
 		}, {
 			track: 'a',
 			lockIcon: 'diamond',
 			code: '2 - 20 - 38',
 			opens: 'yahtzee',
-			box: 'Gray metal box'
+			box: 'Scene It?'
 		}, {
 			track: 'a',
 			lockIcon: 'paw',
 			code: '1 - 9 - 35',
 			opens: 'scrabble',
-			box: 'Gray suitcase'
+			box: 'Life'
 		}, {
 			track: 'a',
-			lockIcon: 'gavel',
-			code: '14 - 29 - 11',
-			opens: 'jigsaw',
-			box: 'Brown bread box'
+			lockIcon: 'rocket',
+			code: '36 - 11 - 37',
+			opens: 'jigsaw', // clue to track c
+			box: 'Blue Dice Cube'
 		}, {
 			track: 'b',
 			lockIcon: 'anchor',
 			code: '30 - 5 - 33',
 			opens: 'clue',
-			box: 'Blue dice block'
+			box: 'Brown Bread Box'
 		}, {
 			track: 'b',
 			lockIcon: 'tachometer',
@@ -42,57 +42,57 @@ escapeApp.factory('LockFactory', [
 			track: 'b',
 			lockIcon: 'tree',
 			code: '24 - 4 - 27',
-			opens: 'chess',
-			box: 'Rush Hour'
+			opens: 'chess', // by way of Chutes and Ladders
+			box: 'Blurt'
 		}, {
 			track: 'b',
-			lockIcon: 'rocket',
-			code: '36 - 11 - 37',
+			lockIcon: 'gavel',
+			code: '14 - 29 - 11',
 			opens: 'taboo',
 			box: 'Pictionary'
 		// }, {
 		// 	track: 'b',
-		// 	lockIcon: 'birthday-cake',
-		// 	code: '30 - 38 - 24',
+		// lockIcon: 'umbrella',
+		// code: '1 - 15 - 0',
 		// 	opens: 'crossword',
 		// 	box: 'Tripoley'
 		}, {
 			track: 'b',
 			lockIcon: 'globe',
 			code: '18 - 25 - 7',
-			opens: 'jigsaw',
-			box: 'Scene It?'
+			opens: 'jigsaw', // clue to track a
+			box: 'Trivial Pursuit'
 		}, {
 			track: 'c',
-			lockIcon: 'umbrella',
-			code: '1 - 15 - 0',
+			lockIcon: 'birthday-cake',
+			code: '30 - 38 - 24',
 			opens: 'connect4',
-			box: 'Cranium'
+			box: 'Mancala'
 		}, {
 			track: 'c',
 			lockIcon: 'bomb',
 			code: '23 - 7 - 26',
 			opens: 'fiveRoutes',
-			box: 'Life'
+			box: 'Cranium'
 		}, {
 			track: 'c',
 			lockIcon: 'motorcycle',
 			code: '16 - 21 - 39',
 			opens: 'wordFind',
-			box: 'Blurt'
+			box: 'Gray Metal Box'
 		}, {
 			track: 'c',
 			lockIcon: 'ticket',
 			code: '11 - 21 - 37',
-			opens: 'jigsaw',
-			box: 'Trivial Pursuit'
+			opens: 'jigsaw', // clue to track b
+			box: 'Rush Hour'
 		}];
 	}
 ]);
 
 //	FYI
 // scissors (code arrived from chutes and ladders)
-// NOTE: the clue for chutes and ladders in in the umbrella locked Cranium
+// NOTE: the clue for chutes and ladders in in the tree locked Blurt
 // code: '28 - 36 - 16',
-// box: mancala
-// opens: on the way to connect4
+// box: Gray Suitcase
+// opens: on the way to chess
